fd7763dce1149c54e82ac079e4ba30aa5b9b9713
Júlio Cézar <soulblighter99@yahoo.com.br>
2019-01-01 22:06:03 -0200

    Dagger update #1
    Signed-off-by: Júlio Cézar <soulblighter99@yahoo.com.br>

--- app/src/main/java/br/com/soulblighter/popularmoviesapp/PopularMoviesApp.java
+++ app/src/main/java/br/com/soulblighter/popularmoviesapp/PopularMoviesApp.java
@@ -2,29 +2,29 @@ package br.com.soulblighter.popularmoviesapp;
 
 import android.app.Application;
 
+import br.com.soulblighter.popularmoviesapp.dagger2.AppComponent;
+import br.com.soulblighter.popularmoviesapp.dagger2.DaggerAppComponent;
 import br.com.soulblighter.popularmoviesapp.dagger2.AppModule;
-import br.com.soulblighter.popularmoviesapp.dagger2.DaggerNetComponent;
-import br.com.soulblighter.popularmoviesapp.dagger2.NetComponent;
 import br.com.soulblighter.popularmoviesapp.dagger2.NetModule;
 
 public class PopularMoviesApp extends Application {
 
-    private NetComponent mNetComponent;
+    private AppComponent mAppComponent;
 
     public static final String BASE_URL = "http://api.themoviedb.org/3/";
 
     @Override
     public void onCreate() {
         super.onCreate();
-        mNetComponent = createRetrofitComponent();
+        mAppComponent = createRetrofitComponent();
     }
 
-    public NetComponent getDaggerRetrofitComponent() {
-        return mNetComponent;
+    public AppComponent getDaggerRetrofitComponent() {
+        return mAppComponent;
     }
 
-    private NetComponent createRetrofitComponent() {
-        return DaggerNetComponent
+    private AppComponent createRetrofitComponent() {
+        return DaggerAppComponent
                 .builder()
                 .appModule(new AppModule(this))
                 .netModule(new NetModule(BASE_URL))
--- app/src/main/java/br/com/soulblighter/popularmoviesapp/dagger2/AppComponent.java
+++ app/src/main/java/br/com/soulblighter/popularmoviesapp/dagger2/AppComponent.java
@@ -11,12 +11,7 @@ import retrofit2.Retrofit;
 
 @Singleton
 @Component(modules = {AppModule.class, NetModule.class})
-public interface NetComponent {
-    // downstream components need these exposed with the return type
-    // method name does not really matter
-    Retrofit retrofit();
-    Application app();
-
+public interface AppComponent {
     void inject(MainActivity main);
     void inject(DetailActivity detail);
 }
